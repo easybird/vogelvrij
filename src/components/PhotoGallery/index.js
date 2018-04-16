3e8b23669a54683ae6ce5ef3dc6adfc36555c3d2
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2018-04-17 01:35:34 +0200

    workaround for ZMage library that is unable to render serverside

--- src/components/PhotoGallery/index.js
+++ src/components/PhotoGallery/index.js
@@ -1,14 +1,18 @@
 import React, { Fragment } from 'react';
 import galleryStyle from './gallery.css';
 import Img from 'gatsby-image';
-import ZMage from 'react-zmage';
 
 const PhotoGallery = ({ children }) => <section className="gallerySection">{children}</section>;
+
+if (typeof window !== `undefined`) {
+  const ZMage = require('react-zmage');
+}
 
 export const Photo = ({ style, title, sizes, resolutions, className }) => (
   <div className="photo-zoom">
     <Img style={style} key={title} outerWrapperClassName="gallery" className={className || 'image'} sizes={sizes} />
-    {resolutions && <ZMage style={{ opacity: 0, position: 'absolute', top: 0, left: 0 }} src={resolutions.src} />}
+    {typeof ZMage !== `undefined` &&
+      resolutions && <ZMage style={{ opacity: 0, position: 'absolute', top: 0, left: 0 }} src={resolutions.src} />}
   </div>
 );
 

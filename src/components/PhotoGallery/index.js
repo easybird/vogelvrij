1225867674dc5cc09fb782d41a6480edc8a7791b
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2018-04-18 12:24:45 +0200

    fix ZMage require when window undefined

--- src/components/PhotoGallery/index.js
+++ src/components/PhotoGallery/index.js
@@ -2,17 +2,17 @@ import React, { Fragment } from 'react';
 import galleryStyle from './gallery.css';
 import Img from 'gatsby-image';
 
+let ZMage = null;
 const PhotoGallery = ({ children }) => <section className="gallerySection">{children}</section>;
 
 if (typeof window !== `undefined`) {
-  const ZMage = require('react-zmage');
+  ZMage = require('react-zmage').default;
 }
 
 export const Photo = ({ style, title, sizes, resolutions, className }) => (
   <div className="photo-zoom">
     <Img style={style} key={title} outerWrapperClassName="gallery" className={className || 'image'} sizes={sizes} />
-    {typeof ZMage !== `undefined` &&
-      resolutions && <ZMage style={{ opacity: 0, position: 'absolute', top: 0, left: 0 }} src={resolutions.src} />}
+    {ZMage && resolutions && <ZMage style={{ opacity: 0, position: 'absolute', top: 0, left: 0 }} src={resolutions.src} />}
   </div>
 );
 

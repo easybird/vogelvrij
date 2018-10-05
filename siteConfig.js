64d9d50a2dac5e178d9e7f32714402b5af1f16f7
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2018-10-05 12:55:25 +0200

    add google-site-verification

--- siteConfig.js
+++ siteConfig.js
@@ -7,5 +7,6 @@ export default {
         'In mijn ogen is een juweel is niet zomaar wat opsmuk. Het is een persoonlijk sieraad, iets wat bij jou als persoon past, met een betekenis. Je koopt het bij een gebeurtenis, een mijlpaal in je leven, … Je koopt het voor jezelf, of voor een dierbaar iemand.',
     },
     { name: 'keywords', content: 'juwelen, juwelry' },
+    { name: 'google-site-verification', content: 'UnzsU2I7fX_birO_vw86eHv9AkT2lgMQ-4um0Ozt1MY'}
   ],
 };

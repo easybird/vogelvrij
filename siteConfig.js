a28ac4d5f18d083b4a8568a748754fd6b1f5c620
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2018-11-18 20:03:32 +0100

    add favicon

--- siteConfig.js
+++ siteConfig.js
@@ -6,7 +6,13 @@ export default {
       content:
         'Handgemaakte juwelen op maat, met liefde gemaakt vanuit Gent. Als geschenk voor jezelf, maatwerk voor je huwelijk, je verloving, of zomaar, voor een dierbaar iemand. Wil je oud goud recupereren? Dat kan ook.',
     },
-    { name: 'keywords', content: 'juwelen, juwelry, gent, ghent, huwelijk, trouwen, verloofd' },
-    { name: 'google-site-verification', content: 'UnzsU2I7fX_birO_vw86eHv9AkT2lgMQ-4um0Ozt1MY'}
-  ],
+    {
+      name: 'keywords',
+      content: 'juwelen, juwelry, gent, ghent, huwelijk, trouwen, verloofd',
+    },
+    {
+      name: 'google-site-verification',
+      content: 'UnzsU2I7fX_birO_vw86eHv9AkT2lgMQ-4um0Ozt1MY',
+    },
+  ]
 };
--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Helmet from 'react-helmet';
 import config from '../../../siteConfig';
+import favicon32 from '../../images/favicon-32x32.png';
+import favicon192 from '../../images/favicon-192x192.png';
 
 class Head extends Component {
   render() {
@@ -14,10 +16,17 @@ class Head extends Component {
           href="https://fonts.googleapis.com/css?family=Cutive+Mono|Dosis|Josefin+Slab|Muli|Quicksand|Titillium+Web"
           rel="stylesheet"
         />
-         <meta name="viewport" content="width=device-width, initial-scale = 1.0, minimum-scale=1.0, user-scalable=no" />
-         {/* minimum scale did the trick */}
-         <meta name="apple-mobile-web-app-capable" content="yes" />
-         {config.meta.map(({name, content}) => <meta name={name} content={content} />)}
+        <link rel="icon" type="image/png" href={favicon32} sizes="32x32" />
+        <link rel="icon" type="image/png" href={favicon192} sizes="192x192" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale = 1.0, minimum-scale=1.0, user-scalable=no"
+        />
+        {/* minimum scale did the trick */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        {config.meta.map(({name, content}) => (
+          <meta name={name} content={content} />
+        ))}
       </Helmet>
     );
   }

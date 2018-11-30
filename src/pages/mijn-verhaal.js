90bdbdf3a3ad739889987154f936ac2e03b57f35
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2018-11-30 17:25:12 +0100

    fix typo

--- src/pages/mijn-verhaal.js
+++ src/pages/mijn-verhaal.js
@@ -1,39 +1,47 @@
 import React from 'react';
 import {Helmet} from 'react-helmet';
 import './basic.css';
-import { Photo } from '../components/PhotoGallery';
+import {Photo} from '../components/PhotoGallery';
 
-const MijnVerhaal = ({ data: { allContentfulPage: { edges } } }) => (
+const MijnVerhaal = ({data: {allContentfulPage: {edges}}}) => (
   <div className="basic-container">
-    [<Helmet key="helmet">
-      <title>VogelVrij, als in puur, natuurlijk & vol passie</title>
-    </Helmet>,
-    {edges.map(({ node: { intro, description, hero } }) => [
-
-      hero && (
-        <div className="hero-container">
-          <Photo title={hero.title} sizes={hero.sizes} className="contact-image" />
-        </div>
-      ),
-      <div className="text-container">
-        {intro && (
-          <div
-            className="page"
-            dangerouslySetInnerHTML={{
-              __html: intro.childMarkdownRemark && intro.childMarkdownRemark.html,
-            }}
-          />
-        )}
-        {description && (
-          <div
-            className="quotedBy"
-            dangerouslySetInnerHTML={{
-              __html: description.childMarkdownRemark && description.childMarkdownRemark.html,
-            }}
-          />
-        )}
-      </div>,
-    ])}]
+    {[
+      <Helmet key="helmet">
+        <title>VogelVrij, als in puur, natuurlijk & vol passie</title>
+      </Helmet>,
+      edges.map(({node: {intro, description, hero}}) => [
+        hero && (
+          <div className="hero-container">
+            <Photo
+              title={hero.title}
+              sizes={hero.sizes}
+              className="contact-image"
+            />
+          </div>
+        ),
+        <div className="text-container">
+          {intro && (
+            <div
+              className="page"
+              dangerouslySetInnerHTML={{
+                __html:
+                  intro.childMarkdownRemark && intro.childMarkdownRemark.html,
+              }}
+            />
+          )}
+          {description && (
+            <div
+              className="quotedBy"
+              dangerouslySetInnerHTML={{
+                __html:
+                  description.childMarkdownRemark &&
+                  description.childMarkdownRemark.html,
+              }}
+            />
+          )}
+        </div>,
+      ]),
+    ]}
   </div>
 );
 
@@ -41,7 +49,7 @@ export default MijnVerhaal;
 
 export const query = graphql`
   query MijnVerhaalQuery {
-    allContentfulPage(filter: { url: { eq: "mijn-verhaal" } }) {
+    allContentfulPage(filter: {url: {eq: "mijn-verhaal"}}) {
       edges {
         node {
           id

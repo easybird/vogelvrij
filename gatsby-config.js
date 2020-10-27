63a5e968eb2e728ad5af5b4edab5bc6ff89a986c
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2020-10-27 21:41:35 +0100

    add hotjar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
-require('dotenv').config({
+require ('dotenv').config ({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
 module.exports = {
   siteMetadata: {
     title: 'Vogelvrij',
-    siteUrl: 'https://vogelvrij.be'
+    siteUrl: 'https://vogelvrij.be',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -25,6 +25,14 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-114741533-1',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-hotjar`,
+      options: {
+        includeInDevelopment: false, // optional parameter to include script in development
+        id: 2064460,
+        sv: 6,
       },
     },
   ],
